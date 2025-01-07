a1c04a74f1a3e1dc32cbddde04cce9a9b2ee361c
venkateshramkumarg <venkateshramkumar.g2021csec@sece.ac.in>
2025-01-07 15:13:55 +0530

    Implement JWT authentication and update blog routes to require user verification

--- jwt_token.py
+++ jwt_token.py
@@ -1,10 +1,14 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
 from datetime import datetime, timedelta, timezone
 import jwt 
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
@@ -15,3 +19,22 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+async def verify_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        return email
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    return verify_token(token)
+
--- routers/blog.py
+++ routers/blog.py
@@ -1,36 +1,39 @@
 from typing import List
 from fastapi import APIRouter,Depends,status
 from sqlalchemy.orm import Session
-import schemas,database
-import repository
+import schemas,database,jwt_token
+from repository import blog
 
 router=APIRouter(
-    tags=['user']
+    tags=['blog']
 )
 
 @router.post('/create_blog', response_model=schemas.BlogResponse,status_code=status.HTTP_201_CREATED)
-def create_blog(request: schemas.BlogCreate, db: Session = Depends(database.get_db)):
-    return repository.blog.create_blog(request,db)
+def create_blog(request: schemas.BlogCreate, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(jwt_token.get_current_user)):
+    return blog.create_blog(request,db)
+
+
+
 
 @router.get('/blogs/', response_model=List[schemas.BlogResponse])
-def get_blogs(db: Session = Depends(database.get_db)):
-    return repository.blog.get_blogs(db)
+def get_blogs(db: Session = Depends(database.get_db), current_user: schemas.User = Depends(jwt_token.get_current_user)):
+    return blog.get_blogs(db)
 
 
 @router.get('/blog/{blog_id}', response_model=schemas.BlogResponse)
-def get_blog(blog_id: int, db: Session = Depends(database.get_db)):
-    return repository.blog.get_blog(blog_id,db)
+def get_blog(blog_id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(jwt_token.get_current_user)):
+    return blog.get_blog(blog_id,db)
 
 
 
 @router.put('/edit_blog/{blog_id}',response_model=schemas.BlogResponse,status_code=status.HTTP_202_ACCEPTED)
-def edit_blog(blog_id: int, request: schemas.BlogCreate, db: Session = Depends(database.get_db)):
-    return repository.blog.edit_blog(blog_id,request,db)
+def edit_blog(blog_id: int, request: schemas.BlogCreate, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(jwt_token.get_current_user)):
+    return blog.edit_blog(blog_id,request,db)
 
 
 @router.delete('/blog/{blog_id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(blog_id:int,db:Session=Depends(database.get_db)):
-    return repository.blog.delete_blog(blog_id,db)
+def delete_blog(blog_id:int,db:Session=Depends(database.get_db), current_user: schemas.User = Depends(jwt_token.get_current_user)):
+    return blog.delete_blog(blog_id,db)
 
 
 
